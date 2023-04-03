3326087d0e98c9a554af568f79a966df79d9786a
Your Name <achmadfadilla112@gmail.com>
2023-04-03 12:22:30 +0700

    tes deploy

--- index.js
+++ index.js
@@ -3,33 +3,40 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const morgan = require("morgan")
-const app = express();
+const index = express();
 const router = require("./routes");
 const path = require('path')
 
 require('./db/db')
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(router);
-app.use(express.json());
-app.set("view engine", "ejs");
+index.use(express.json());
+index.use(express.urlencoded({ extended: true }));
+index.use(cors());
+index.use(router);
+index.use(express.json());
+index.set("view engine", "ejs");
 // app.use(express.static(path.join(__dirname, "client")));
-app.use(morgan("dev"));
+index.use(morgan("dev"));
 
 // const uri = 'mongodb://127.0.0.1:27017';
 
 const { HTTP_PORT } = process.env;
 
-app.use((req, res, next) => {
+index.get('/', (req, res) => {
+    return res.status(200).json({
+        status: true,
+        message: 'ini percobaan deploy ke vercel'
+    })
+})
+
+index.use((req, res, next) => {
     return res.status(404).json({
         status: false,
         message: 'hayo tersesat ya?'
     })
 })
 
-app.use((err, req, res, next) => {
+index.use((err, req, res, next) => {
     if(err.code == 'LIMIT_FILE_SIZE' || err.message == 'file too large'){
         return res.status(500).json({
             status: false,
@@ -45,4 +52,4 @@ app.use((err, req, res, next) => {
 
 
 
-app.listen(HTTP_PORT, () => console.log("listening on port", HTTP_PORT));
+index.listen(HTTP_PORT, () => console.log("listening on port", HTTP_PORT));
