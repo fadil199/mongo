74dafa5229763e8f764fc98b9faef8bc26916e80
Your Name <achmadfadilla112@gmail.com>
2023-04-03 13:39:33 +0700

    test

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/tes",{ 
+mongoose.connect("mongodb+srv://achmadfadilla112:19maret1995@cluster0.oeayxrk.mongodb.net/test",{ 
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
